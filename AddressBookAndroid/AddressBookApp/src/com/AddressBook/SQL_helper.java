11c517ca196b33ed49cb1f7e616150157d052ebf
GameEgg OOPArts <whddnjs2860@naver.com>
2013-05-14 08:09:03 +0900

    ss

--- AddressBookAndroid/AddressBookApp/src/com/AddressBook/SQL_helper.java
+++ AddressBookAndroid/AddressBookApp/src/com/AddressBook/SQL_helper.java
@@ -9,13 +9,13 @@ public class SQL_helper extends SQLiteOpenHelper{
 	
 
 	public SQL_helper(Context context) {
-		super(context, "LINK_DB", null, 1);
+		super(context, "LINK.db", null, 1);
 	}
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		db.execSQL("CREATE TABLE data ( id INTEGER PRIMARY KEY AUTOINCREMENT, phone TEXT");
-		db.execSQL("CREATE TABLE field ( id INTEGER, fieldName TEXT, fieldData TEXT");
+		db.execSQL("CREATE TABLE data ( id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, phone TEXT )");
+		db.execSQL("CREATE TABLE field ( id INTEGER PRIMARY KEY AUTOINCREMENT, dataID INTEGER, fieldName TEXT, fieldData TEXT )");
 	}
 
 	@Override
